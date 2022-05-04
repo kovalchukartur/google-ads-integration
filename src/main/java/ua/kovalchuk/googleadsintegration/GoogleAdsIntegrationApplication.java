f884a13cda4931b720560ce972c18d3e2be9bb8a
Artur Kovalchuk <arkovalchuk@playtika.com>
2022-05-04 12:01:14 +0300

    Init

---
+++ src/main/java/ua/kovalchuk/googleadsintegration/GoogleAdsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package ua.kovalchuk.googleadsintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleAdsIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleAdsIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/kovalchuk/googleadsintegration/GoogleAdsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kovalchuk.googleadsintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleAdsIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
